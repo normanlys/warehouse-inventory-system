2dbfbcc785bef62b406aff5a247e0f2e7f31352a
Norman <normanlys16@gmail.com>
2020-10-10 19:42:00 +0800

    Move file

--- WarehouseInventory/src/main/java/com/warehouse/web/WarehouseInventoryApplication.java
+++ WarehouseInventory/src/main/java/com/warehouse/web/WarehouseInventoryApplication.java
@@ -1,4 +1,4 @@
-package com.example.WarehouseInventory;
+package com.warehouse.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
