1de84f905e6d756cbd01f3245d02856b8de3d888
Norman <normanlys16@gmail.com>
2020-10-11 13:56:57 +0800

    Allow cors globally

--- WarehouseInventory/src/main/java/com/warehouse/rest/RestApplication.java
+++ WarehouseInventory/src/main/java/com/warehouse/rest/RestApplication.java
@@ -2,10 +2,24 @@ package com.warehouse.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
     }
+    
+    // allow cors
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
